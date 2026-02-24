44e615874c35eb847beaa59f4a6ab197f35cb435
Endriw Bento <endriwbento@gmail.com>
2026-02-24 16:34:01 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -43,8 +43,7 @@ public class UI {
             char colum = s.charAt(0);
             int row = Integer.parseInt(s.substring(1));
             return new ChessPosition(colum, row);
-        }
-        catch (RuntimeException e) {
+        } catch (RuntimeException e) {
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
         }
     }
@@ -55,9 +54,14 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn :" + chessMatch.getTurn());
-        System.out.println("Waiting player :" + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()) {
-            System.out.println("CHECK!");
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Waiting player :" + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                System.out.println("CHECK!");
+            }
+        } else {
+            System.out.println("CHECKMATE!");
+            System.out.println("Winner :" + chessMatch.getCurrentPlayer());
         }
     }
 
@@ -72,7 +76,7 @@ public class UI {
         System.out.println("  a b c d e f g h");
     }
 
-    public static void printBoard(ChessPiece[][] pieces, boolean [][] possibleMoves) {
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces[i].length; j++) {
@@ -89,12 +93,10 @@ public class UI {
         }
         if (piece == null) {
             System.out.print("-" + ANSI_RESET);
-        }
-        else {
+        } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
+            } else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
